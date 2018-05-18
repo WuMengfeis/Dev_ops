0ac816b6efaa906d9f2d400d0d3be819315fafc5
WuMengfei666 <38835864+WuMengfei666@users.noreply.github.com>
2018-05-18 22:31:45 +0800

    试用期第二周任务

---
+++ Task-试用期第二周/first.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# this is my first shell
+echo "please enter your name"
+read NAME
+echo "hello ,$NAME"
+echo "please enter your you age"
+read AGE
+echo "your age is :$AGE"
+echo "please enter your sex"
+read SEX
+echo "your sex is :$SEX"
